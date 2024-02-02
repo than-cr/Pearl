3abfe4b78c1d295dd76bc3f42aa022252b9fbf5c
Jonathan Castro <than.cr@outlook.com>
2024-02-01 20:49:04 -0600

    Fix content mixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -18,10 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(UrlGenerator $url): void
+    public function boot(): void
     {
         if(env('APP_ENV') !== 'local'){
-            $url->forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
